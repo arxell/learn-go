7c87a95fc3cf4084a8b28c32f0419534311bb56b
Anton Ogorodnikov <anton.ogorodnikov@ostrovok.ru>
2016-07-13 23:12:25 +0300

    fix

--- learn-go/hello.go
+++ learn-go/hello.go
@@ -17,7 +17,7 @@ import (
 var DB = make(map[string]string)
 
 type ApiAuthKey struct {
-	ID        uint
+	ID        int
 	Key       string
 	Is_Active bool
 	Rps       int
@@ -35,12 +35,25 @@ type Config struct {
 	DatabasePassword string `env:"DatabasePassword"`
 }
 
+func get_apiauthkeys(DB *gorm.DB) map[string]string {
+	apiauthkeys := []ApiAuthKey{}
+	DB.Find(&apiauthkeys)
+
+	m := map[string]string{}
+	for _, apiauthkey := range apiauthkeys {
+		m[strconv.Itoa(apiauthkey.ID)] = apiauthkey.Key
+	}
+	return m
+}
+
 func main() {
 
+	// Load env config
 	cfg := Config{}
 	env.Parse(&cfg)
 	fmt.Println(cfg)
 
+	// Init database
 	DB, err := gorm.Open(
 		"postgres",
 		"host="+cfg.DatabaseHost+
@@ -54,24 +67,48 @@ func main() {
 	}
 	router := gin.Default()
 
-	// Ping test
-	router.GET("/ping", func(c *gin.Context) {
-		ois := []ApiAuthKey{}
-		DB.Limit(10).Find(&ois)
-		fmt.Println(ois)
-		c.String(200, "pong")
-	})
-
-	// Initalize
+	//	=== Admin part START===
 	Admin := admin.New(&qor.Config{DB: DB})
-
-	// Create resources from GORM-backend model
+	//	Create resources from GORM-backend model
 	Admin.AddResource(&ApiAuthKey{})
-
-	// Binding qor admin with Gin
+	//	Binding admin with Gin
 	mux := http.NewServeMux()
 	Admin.MountTo("/admin", mux)
 	router.Any("/admin/*w", gin.WrapH(mux))
+	//	=== Admin part END ===
+
+	// Init HTTP BASIC AUTH
+	authorized := router.Group("/api", gin.BasicAuth(get_apiauthkeys(DB)))
+
+	// === Endpoints part START ===
+	// Ping test
+	router.GET("/ping", func(c *gin.Context) {
+		c.String(200, "pong")
+	})
+	// test json result
+	router.GET("/ping/json", func(c *gin.Context) {
+		// You also can use a struct
+		var msg struct {
+			Name    string `json:"user"`
+			Message string
+			Number  int
+		}
+		msg.Name = "Lena"
+		msg.Message = "hey"
+		msg.Number = 123
+		c.JSON(http.StatusOK, msg)
+	})
+	// hotel/rates
+	authorized.GET("/hotel/rates", func(c *gin.Context) {
+		// get user, it was set by the BasicAuth middleware
+		user := c.MustGet(gin.AuthUserKey).(string)
+		if secret, ok := secrets[user]; ok {
+			c.JSON(http.StatusOK, gin.H{"user": user, "secret": secret})
+		} else {
+			c.JSON(http.StatusOK, gin.H{"user": user, "secret": "NO SECRET :("})
+		}
+	})
+	// === Endpoints part END ===
 
 	s := &http.Server{
 		Addr:           ":8080",
